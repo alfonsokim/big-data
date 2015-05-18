912cdc01bb6dceff7558c633c6566933ed6549f4
Alfonso Kim <alfonsokim84@gmail.com>
2015-05-18 00:29:59 -0500

    avances proyecto 2

--- alumnos/alfonsokim/proyecto_2/gdelt/load_postgres.sh
+++ alumnos/alfonsokim/proyecto_2/gdelt/load_postgres.sh
@@ -2,6 +2,6 @@
 
 PG_USER=postgres
 PG_DATABASE=ufo
-PG_TABLE=gdelt_historic
+PG_TABLE=gdelt_reduced
 
-unzip -p $1 | psql -U $PG_USER -d $PG_DATABASE -c "\copy $PG_TABLE from STDIN with csv delimiter E'\t'"
+unzip -p $1 | ./filter_columns.sh | psql -U $PG_USER -d $PG_DATABASE -c "\copy $PG_TABLE from STDIN with csv delimiter E'\t'"
